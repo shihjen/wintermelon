72a701c0a1e752859d55a786d13f6877bb8e2b10
shihjen <97783251+shihjen@users.noreply.github.com>
2022-12-31 11:35:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,18 @@
   // Total seconds to wait
     var seconds = 10;
     
-    function countdown1() {
+    function countdown() {
         seconds = seconds - 1;
         if (seconds < 0) {
             // Chnage your redirection link here
             window.location = "https://duckdev.com";
         } else {
             // Update remaining seconds
-            document.getElementById("countdown1").innerHTML = seconds;
+            document.getElementById("countdown").innerHTML = seconds;
             // Count down using javascript
-            window.setTimeout("countdown1()", 1000);
+            window.setTimeout("countdown()", 1000);
         }
     }
     
     // Run countdown function
-    countdown1();
+    countdown();
