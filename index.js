49c25519d1b3ec7801c7b5c1625766b8f9e960dd
shihjen <97783251+shihjen@users.noreply.github.com>
2022-12-31 11:14:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ const countdown = () => {
 // should use 500 as setInterval won't always run on time.
 setInterval(countdown, 500);
 
-
-  
+  // Total seconds to wait
+    var seconds = 10;
+    
+    function countdown1() {
+        seconds = seconds - 1;
+        if (seconds < 0) {
+            // Chnage your redirection link here
+            window.location = "https://duckdev.com";
+        } else {
+            // Update remaining seconds
+            document.getElementById("countdown1").innerHTML = seconds;
+            // Count down using javascript
+            window.setTimeout("countdown1()", 1000);
+        }
+    }
+    
+    // Run countdown function
+    countdown1();
